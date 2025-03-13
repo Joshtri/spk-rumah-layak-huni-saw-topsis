c0f73dce39d5673a0253b22f65ebd91dfe8d7d73
Anthasss <anthasprog@gmail.com>
2025-03-14 05:51:50 +0800

    refactor IconCards and DashboardPage components; improve styling and layout for better visual consistency

--- src/components/iconCards.jsx
+++ src/components/iconCards.jsx
@@ -2,13 +2,20 @@ import { Card } from "flowbite-react";
 
 export default function IconCards({ title, description, icon }) {
   return (
-    <Card className="max-w-md w-full [&>div]:bg-amber-500">
+    <Card
+      className="max-w-md w-full"
+      theme={{
+        root: {
+          base: "bg-white border border-gray-200 rounded-lg shadow transition-shadow duration-300 hover:shadow-md hover:shadow-stone-500 dark:bg-gray-800 dark:border-gray-700",
+        },
+      }}
+    >
       <div className="grid grid-cols-[auto_1fr] gap-4">
-        {icon && <div className="flex items-center justify-center">{icon}</div>}
+        {icon && <div className="flex items-center justify-center text-white">{icon}</div>}
 
         <div className="flex flex-col justify-center">
-          {title && <h5 className="text-xl font-bold">{title}</h5>}
-          {description && <p className="text-gray-600">{description}</p>}
+          {title && <h5 className="text-xl font-bold text-white">{title}</h5>}
+          {description && <p className="text-white">{description}</p>}
         </div>
       </div>
     </Card>
--- src/pages/dashboardPage.jsx
+++ src/pages/dashboardPage.jsx
@@ -5,14 +5,14 @@ import { Home } from "lucide-react";
 export default function DashboardPage() {
   return (
     <LayoutRoot>
-      <div className="border-2 border-green-500 grid grid-rows-[auto,1fr] gap-6 h-full">
+      <div className=" grid grid-rows-[auto,1fr] gap-10 h-full">
         {/* greetings */}
-        <div className="border-red-500 border-2 flex justify-center items-center h-full">
+        <div className="flex justify-center items-center h-full text-center">
           <h1 className="text-6xl font-bold">Selamat Datang!</h1>
         </div>
 
         {/* cards */}
-        <div className="border-blue-500 border-2 grid grid-cols-1 md:grid-cols-3 gap-4 h-full">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 h-full">
           <IconCards
             title={"Hello"}
             description={"This is a card that contains title, desc, and icon"}
