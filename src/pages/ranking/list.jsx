877757267833b99c7855ca3557bebc2f52e25b86
Anthasss <anthasprog@gmail.com>
2025-03-15 21:51:56 +0800

    feat: add ranking page and table component

---
+++ src/pages/ranking/list.jsx
@@ -0,0 +1,10 @@
+import LayoutRoot from "../layout";
+import RankingTable from "@/components/ranking/table";
+
+export default function RankingList() {
+  return (
+    <LayoutRoot>
+      <RankingTable />
+    </LayoutRoot>
+  );
+}
