c1db812659416b0729e6ba5c31674f2f21fa619a
Joshtri <stuffofyos1516@gmail.com>
2025-03-19 22:51:50 +0700

    refactor: comment out ProtectedRoutes and adjust routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ProtectedRoutes from "./Components/Addons/ProtectedRoutes";
+// import ProtectedRoutes from "./Components/Addons/ProtectedRoutes";
 import LoginPage from "./Pages/Auth/LoginPage";
 import DashboardPage from "./Pages/Dashboard/Dashboard";
 import KriteriaList from "./Pages/Kriteria/KriteriaList";
@@ -17,67 +17,31 @@ import AlternatifPeriodeList from "./Pages/AlternatifPeriode/list";
 function App() {
   return (
     <KriteriaProvider>
-      <Toaster
-        position="top-right"
-        richColors
-      />
+      <Toaster position="top-right" richColors />
       <BrowserRouter>
         <Routes>
           {/* Public Routes */}
-          <Route
-            path="/"
-            element={<LoginPage />}
-          />
-          <Route
-            path="/forgot-password"
-            element={<ForgotPassword />}
-          />
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
 
           {/* Protected Routes - Semua halaman hanya bisa diakses oleh ADMIN */}
-          <Route element={<ProtectedRoutes />}>
-            <Route
-              path="/dashboard"
-              element={<DashboardPage />}
-            />
-            <Route
-              path="/alternatif"
-              element={<AlternatifList />}
-            />
-            <Route
-              path="/profile"
-              element={<Profile />}
-            />
-            <Route
-              path="/ranking"
-              element={<RankingList />}
-            />
-            <Route
-              path="/kriteria"
-              element={<KriteriaList />}
-            />
-            <Route
-              path="/sub-kriteria"
-              element={<SubKriteriaList />}
-            />
-            <Route
-              path="/periode"
-              element={<PeriodeList />}
-            />
-            <Route
-              path="/users-management"
-              element={<UsersList />}
-            />
-            <Route
-              path="/alternatif-periode"
-              element={<AlternatifPeriodeList />}
-            />
-          </Route>
+          {/* <Route element={<ProtectedRoutes />}> */}
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/alternatif" element={<AlternatifList />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/ranking" element={<RankingList />} />
+          <Route path="/kriteria" element={<KriteriaList />} />
+          <Route path="/sub-kriteria" element={<SubKriteriaList />} />
+          <Route path="/periode" element={<PeriodeList />} />
+          <Route path="/users-management" element={<UsersList />} />
+          <Route
+            path="/alternatif-periode"
+            element={<AlternatifPeriodeList />}
+          />
+          {/* </Route> */}
 
           {/* Not Found Route */}
-          <Route
-            path="*"
-            element={<h1>Not Found</h1>}
-          />
+          <Route path="*" element={<h1>Not Found</h1>} />
         </Routes>
       </BrowserRouter>
     </KriteriaProvider>
