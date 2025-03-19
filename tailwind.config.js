cfc3808978a999249d8f698da7cb6791de6043a4
Joshtri <stuffofyos1516@gmail.com>
2025-03-19 21:14:04 +0700

    fix: remove Flowbite CSS import and update Tailwind configuration for flowbite-react

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,15 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*. {js,jsx,ts,tsx}", // 
+
+    './node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    // eslint-disable-next-line no-undef
-    require("flowbite/plugin"), // ✅ Use require instead of import()
+    require('flowbite/plugin')
+
   ],
-};
+}
