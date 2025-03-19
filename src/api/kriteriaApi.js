2bd62a971cfac9501af68602abf4aed6947b7838
Joshtri <stuffofyos1516@gmail.com>
2025-03-19 09:02:05 +0700

    feat: add API functions for managing kriteria (CRUD operations)

---
+++ src/api/kriteriaApi.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+
+
+export const getAllKriteria = async () => {
+    const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/api/kriteria`);
+    return response.data;
+};
+
+export const getKriteriaById = async (id) => {
+    const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/api/kriteria/${id}`);
+    return response.data;
+};
+
+export const createKriteria = async (data) => {
+    const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/api/kriteria`, data);
+    return response.data;
+};
+
+export const updateKriteria = async (id, data) => {
+    const response = await axios.put(`${import.meta.env.VITE_BASE_URL}/api/kriteria/${id}`, data);
+    return response.data;
+};
+
+export const deleteKriteria = async (id) => {
+    const response = await axios.delete(`${import.meta.env.VITE_BASE_URL}/api/kriteria/${id}`);
+    return response.data;
+};
