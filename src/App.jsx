e8b0d4f9d871e01535ea51d7101cbeee6f1208d9
Joshtri <stuffofyos1516@gmail.com>
2025-03-19 09:02:13 +0700

    feat: update dependencies and remove unused components for cleaner code

--- src/App.jsx
+++ src/App.jsx
@@ -8,51 +8,29 @@ import AdminList from "./pages/admin/list";
 import Profile from "./pages/profile-management/profile";
 import ForgotPassword from "./pages/auth/forgotPassword";
 import RankingList from "./pages/ranking/list";
-
+import { Toaster } from "sonner";
+import { KriteriaProvider } from "@/contexts/kriteriaContext"; // Import Provider
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={<LoginPage />}
-          />
-          <Route
-            path="/forgot-password"
-            element={<ForgotPassword />}
-          />
-          <Route
-            path="/dashboard"
-            element={<DashboardPage />}
-          />
-          <Route
-            path="/kriteria"
-            element={<KriteriaList />}
-          />
-          <Route
-            path="/sub-kriteria"
-            element={<SubKriteriaList />}
-          />
-          <Route
-            path="/alternatif"
-            element={<AlternatifList />}
-          />
-          <Route
-            path="/admin"
-            element={<AdminList />}
-          />
-          <Route
-            path="/profile"
-            element={<Profile />}
-          />
-          <Route
-            path="/ranking"
-            element={<RankingList />}
-          />
-        </Routes>
-      </BrowserRouter>
+      <KriteriaProvider>
+        <Toaster position="top-right" richColors />
+
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="/kriteria" element={<KriteriaList />} />
+            <Route path="/sub-kriteria" element={<SubKriteriaList />} />
+            <Route path="/alternatif" element={<AlternatifList />} />
+            <Route path="/admin" element={<AdminList />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/ranking" element={<RankingList />} />
+          </Routes>
+        </BrowserRouter>
+      </KriteriaProvider>
     </>
   );
 }
