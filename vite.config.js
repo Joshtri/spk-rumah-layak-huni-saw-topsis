aab8df1ad308b7315f9f918bcca6177f313a84a5
Joshtri <stuffofyos1516@gmail.com>
2025-06-08 11:02:43 +0700

    feat: add server proxy configuration for API requests in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://spk-inthan-api.vercel.app', // Ganti dengan URL backend Anda
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
